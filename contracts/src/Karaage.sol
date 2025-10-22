a2146131a6d750d33d3fd11f920a42fa7e44065c
Uttkarsh Pawan <utkbest@gmail.com>
2025-10-22 22:04:44 +0530

    add price feeds

--- contracts/src/Karaage.sol
+++ contracts/src/Karaage.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.8.24;
 
 contract Karaage{
-    
+    struct User {
+        uint256 collateral;
+        uint256 debt;
+    }
+
+    //deposit function
+    function depositCollateral() external {
+        
+    }
+    //borrow function
 }
